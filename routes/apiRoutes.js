49d34bf857f066dffd766b13f24d278721a59fe6
robcabobb <robcabobb@gmail.com>
2022-04-24 06:22:39 -0400

    brought in functions for notedata

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,18 +1,31 @@
-const router = require('express').Router()
+// Dependency
+const router = require("express").Router();
+
+// Pulls in noteData functions
+const noteData = require("../db/noteData");
 
 // GET request
-router.get('/notes', (req, res) {
-
-})
+router.get("/notes", (req, res) => {
+	noteData
+		.getNotes()
+		.then((notes) => res.json(notes))
+		.catch((err) => res.status(500).json(err));
+});
 
 // POST request
-router.post('/notes', (req, res) => {
-
-})
+router.post("/notes", (req, res) => {
+	noteData
+		.postNote(req.body)
+		.then((note) => res.json(note))
+		.catch((err) => res.status(500).json(err));
+});
 
 // DELETE request
-router.delete('/notes/:id', (req, res) {
+router.delete("/notes/:id", (req, res) => {
+	noteData
+		.deleteNote(req.params.id)
+		.then(() => res.json({ ok: true }))
+		.catch((err) => res.status(500).json(err));
+});
 
-})
-
-module.exports = router
+module.exports = router;
