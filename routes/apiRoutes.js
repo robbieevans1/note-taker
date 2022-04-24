1b269492a80e995acb4ce3a3af0d4be4489d3e0b
robcabobb <robcabobb@gmail.com>
2022-04-24 06:07:39 -0400

    added shell code for apiroutes, need to work on logic

---
+++ routes/apiRoutes.js
@@ -0,0 +1,16 @@
+const router = require('express').Router()
+
+// GET request
+router.get('/notes', (req, res) {
+
+})
+
+router.post('/notes', (req, res) => {
+
+})
+
+router.delete('/notes/:id', (req, res) {
+
+})
+
+module.exports = router
